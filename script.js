27ba4ead21e0c596c456e5dde3090a3f00a663de
blsmk250127082-beep <blsmk.250127082@budiluhur.sch.id>
2025-11-06 14:47:09 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const learnBtn = document.querySelector('.btn[href="#testimoni"]');
+if (learnBtn) {
+    learnBtn.addEventListener("click", e =>{
+        e.preventDefault();
+        const target = document.querySelector("#testimoni");
+        if (target) {
+        target.scrollIntoView({behavior: "smooth"});
+        }
+    })
+}
+
+
+const form = document.querySelector('.contact-form');
+if (form) {
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        alert("Pesan berhasil di kirim!");
+        form.requestFullscreen();
+})
+}
+
